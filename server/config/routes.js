90207eed23e3a4e8a71aefaed7149b082eef7607
bvautour <vautour.brad@gmail.com>
2017-10-17 15:45:12 -0700

    backend final ready for merge with front end

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,16 +1,20 @@
-//controllers go here
-var model = require('../models/model.js');
+//routes
 
 module.exports = function(app){
 
   app.get('/', function (req, res) {
-    // res.json(models);
-    res.render('server-test', {data: model});
+    res.render('landing');
   });
 
   app.get('/home', function (req, res) {
-    var data;
-    data = 1;
-    res.render('server-test-main');
+    res.render('index');
+  });
+
+  app.get('/about', function (req, res) {
+    res.render('about');
+  });
+
+  app.get('/team', function (req, res) {
+    res.render('team');
   });
 }
